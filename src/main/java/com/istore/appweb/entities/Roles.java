155c97c3d62df496d9545d977928655efe47977f
Danilo-Punk <Danielcerazo19@gmail.com>
2025-09-27 12:17:25 -0500

    c3..creacion de estidades roles

---
+++ src/main/java/com/istore/appweb/entities/Roles.java
@@ -0,0 +1,35 @@
+package com.istore.appweb.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Roles")
+public class Roles {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer idRol;
+
+  private String nombre;
+
+  private Integer nivel;
+
+  private LocalDateTime fechaCreacion = LocalDateTime.now();
+
+  private Boolean eliminado = false;
+
+}
